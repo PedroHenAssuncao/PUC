be88e3b785ff19481c34d809a6d075677361ac54
Pedro Assunção <73478225+PedroHenAssuncao@users.noreply.github.com>
2023-05-24 22:05:12 -0300

    Metade da Lista 5 está pronta

---
+++ ATP/Lista5/Lista5/Lista5/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lista5;
+
+Console.WriteLine("Hello, World!");
+//questao1.rodar();
+//questao2.rodar();
+//questao3.rodar();
+//questao4.rodar();
+questao5.rodar();
---
+++ ATP/Lista5/Lista5/Lista5/questao2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista5
+{
+    public static class questao2
+    {
+        public static void rodar()
+        {
+            float[] notas = new float[10];
+
+            PreencherNotas(notas);
+            CalcularMediaEContarAcimaMedia(notas);
+        }
+
+        public static void PreencherNotas(float[] notas)
+        {
+            Console.WriteLine("Informe as notas dos alunos:");
+
+            for (int i = 0; i < notas.Length; i++)
+            {
+                Console.Write($"Nota do aluno {i + 1}: ");
+                float nota = float.Parse(Console.ReadLine());
+                notas[i] = nota;
+            }
+        }
+
+        public static void CalcularMediaEContarAcimaMedia(float[] notas)
+        {
+            float soma = 0;
+            int countAcimaMedia = 0;
+
+            foreach (float nota in notas)
+            {
+                soma += nota;
+            }
+
+            float media = soma / notas.Length;
+
+            foreach (float nota in notas)
+            {
+                if (nota > media)
+                {
+                    countAcimaMedia++;
+                }
+            }
+
+            Console.WriteLine($"Média da turma: {media}");
+            Console.WriteLine($"Alunos com nota acima da média: {countAcimaMedia}");
+        }
+    }
+}

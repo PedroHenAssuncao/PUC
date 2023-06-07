a62b2fdbbda925aac3c16811b523af37c521c280
Pedro Assunção <73478225+PedroHenAssuncao@users.noreply.github.com>
2023-06-07 11:39:40 -0300

    Lista 6 Completa

---
+++ ATP/Lista6/Lista6/Aluno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista6
+{
+    public class Aluno
+    {
+        public string Matricula { get; set; }
+        public string Telefone { get; set; }
+
+        public Aluno(string matricula, string telefone)
+        {
+            Matricula = matricula;
+            Telefone = telefone;
+        }
+    }
+}
---
+++ ATP/Lista6/Lista6/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lista6;
+
+Console.WriteLine("Hello, World!");
+//questao1.rodar();
+//questao2.rodar();
+//questao3.rodar();
+//questao4.rodar();
+//questao5.rodar();
+//questao6.rodar();
+//questao7.rodar();
+//questao8.rodar();
+//questao9.rodar();
+questao10.rodar();

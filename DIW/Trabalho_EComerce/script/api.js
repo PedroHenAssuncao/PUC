f85fa5f0c4ec7fcafc731448b751b345e926a097
Pedro Assunção <73478225+PedroHenAssuncao@users.noreply.github.com>
2023-06-07 22:27:22 -0300

    pendente apenas pesquisa por texto

--- DIW/Trabalho_EComerce/script/api.js
+++ DIW/Trabalho_EComerce/script/api.js
@@ -1,3 +1,25 @@
+function loadCategories() {
+    fetch('https://fakestoreapi.com/products/categories')
+        .then(res => res.json())
+        .then(json => {
+            let idContainer = 'selectTiposProdutos'
+            let container = document.getElementById(idContainer)
+
+            json.forEach(item => {
+                // Criar um elemento <option> para cada item
+                const option = document.createElement('option');
+                option.value = item; // Definir o valor do <option>
+                option.text = item; // Definir o texto do <option>
+
+                // Adicionar o <option> ao <select>
+                container.appendChild(option);
+            });
+        })
+        .catch(error => {
+            console.log('Ocorreu um erro ao carregar o select:', error)
+        })
+}
+
 function getProductsInitialGrid() {
     fetch('https://fakestoreapi.com/products?limit=9')
         .then(res => res.json())
@@ -8,14 +30,22 @@ function getProductsInitialGrid() {
             json.forEach(item => {
                 // Criar elementos HTML para exibir as informações
                 let card = document.createElement('div')
-                card.classList.add('card', 'col-lg-4', 'col-md-4', 'col-sm-6', 'text-center', 'produto','produtoClick')
+                card.classList.add('card', 'col-lg-4', 'col-md-4', 'col-sm-6', 'text-center', 'produto', 'produtoClick')
+                card.setAttribute('data-id', item.id)
+
+                card.addEventListener('click', function () {
+                    // Obter o ID único do atributo de dados do card
+                    var id = this.getAttribute('data-id');
+                    var url = './detalhes.html?id=' + id
+                    window.location.href = url;
+                });
 
                 let cardBody = document.createElement('div')
                 cardBody.classList.add('card-body')
 
                 let imagem = document.createElement('img')
                 imagem.src = item.image
-                imagem.alt = item.description
+                imagem.alt = item.title
                 imagem.classList.add('card-img-top')
 
                 let titulo = document.createElement('h5')
@@ -38,11 +68,11 @@ function getProductsInitialGrid() {
             });
         })
         .catch(error => {
-            console.log('Ocorreu um erro ao obter os dados da API:', error)
+            console.log('Ocorreu um erro ao inicializar o grid:', error)
         })
 }
 
-function getMostViewd(){
+function getMostViewd() {
     fetch('https://fakestoreapi.com/products?limit=3')
         .then(res => res.json())
         .then(json => {
@@ -52,7 +82,15 @@ function getMostViewd(){
             json.forEach(item => {
                 // Criar elementos HTML para exibir as informações
                 let card = document.createElement('div')
-                card.classList.add('card', 'col-lg-4', 'col-md-6', 'col-sm-12', 'text-center','produtoClick')
+                card.classList.add('card', 'col-lg-4', 'col-md-6', 'col-sm-12', 'text-center', 'produtoClick')
+                card.setAttribute('data-id', item.id)
+
+                card.addEventListener('click', function () {
+                    // Obter o ID único do atributo de dados do card
+                    var id = this.getAttribute('data-id');
+                    var url = './detalhes.html?id=' + id
+                    window.location.href = url;
+                });
 
                 let cardBody = document.createElement('div')
                 cardBody.classList.add('card-body')
@@ -62,7 +100,7 @@ function getMostViewd(){
 
                 let imagem = document.createElement('img')
                 imagem.src = item.image
-                imagem.alt = item.description
+                imagem.alt = item.title
                 imagem.classList.add('imgPequena')
 
                 let titulo = document.createElement('p')
@@ -81,11 +119,11 @@ function getMostViewd(){
             });
         })
         .catch(error => {
-            console.log('Ocorreu um erro ao obter os dados da API:', error)
+            console.log('Ocorreu um erro ao Carregar o most viewd:', error)
         })
 }
 
-function getCarroussel(){
+function getCarroussel() {
     fetch('https://fakestoreapi.com/products?limit=3')
         .then(res => res.json())
         .then(json => {
@@ -97,7 +135,7 @@ function getCarroussel(){
                 // Criar elementos HTML para exibir as informações
                 let card = document.createElement('div')
                 card.classList.add('carousel-item')
-                if(itemCont === 0){
+                if (itemCont === 0) {
                     card.classList.add('active')
                 }
 
@@ -115,5 +153,136 @@ function getCarroussel(){
         })
         .catch(error => {
             console.log('Ocorreu um erro ao obter os dados da API:', error)
+        })
+}
+
+function getDetalhes() {
+    var query = location.search.slice(1);
+
+    var partes = query.split('&');
+    console.log(partes)
+    var valor
+
+    partes.forEach(function (parte) {
+        var chaveValor = parte.split('=');
+        var chave = chaveValor[0];
+        valor = chaveValor[1];
+        console.log(chave)
+        console.log(valor)
+    });
+
+    fetch('https://fakestoreapi.com/products/' + valor)
+        .then(res => res.json())
+        .then(json => {
+            let idContainer = 'ContainerNovoCard'
+            let container = document.getElementById(idContainer)
+
+
+            // Criar elementos HTML para exibir as informações
+            let card = document.createElement('div')
+            card.classList.add('card', 'col-12')
+
+            let imagem = document.createElement('img')
+            imagem.src = json.image
+            imagem.alt = json.title
+            imagem.classList.add('card-img-top')
+
+
+            let titulo = document.createElement('h5')
+            titulo.classList.add('card-title')
+            titulo.textContent = json.title;
+
+            let categoria = document.createElement('p')
+            categoria.classList.add('card-text')
+            categoria.textContent = json.category
+
+            let detalhes = document.createElement('p')
+            detalhes.classList.add('card-text')
+            detalhes.textContent = json.description
+
+            let preco = document.createElement('p')
+            preco.classList.add('card-text')
+            preco.textContent = 'Preço: $' + json.price
+
+            // Adicionar elementos ao card
+            card.appendChild(titulo)
+            card.appendChild(imagem)
+            card.appendChild(categoria)
+            card.appendChild(preco)
+            card.appendChild(detalhes)
+
+            // Adicionar o card ao container
+            container.appendChild(card)
+
+        })
+        .catch(error => {
+            console.log('Ocorreu um erro ao obter os detalhes do produto:', error)
+        })
+}
+
+function getProductsSearchCategory() {
+    var query = location.search.slice(1);
+
+    var partes = query.split('&');
+    console.log(partes)
+    var valor
+
+    partes.forEach(function (parte) {
+        var chaveValor = parte.split('=');
+        var chave = chaveValor[0];
+        valor = chaveValor[1];
+        console.log(chave)
+        console.log(valor)
+    });
+
+    fetch('https://fakestoreapi.com/products/category/' + valor)
+        .then(res => res.json())
+        .then(json => {
+            let idContainer = 'containerCardsPesquisa'
+            let container = document.getElementById(idContainer)
+
+            json.forEach(item => {
+                // Criar elementos HTML para exibir as informações
+                let card = document.createElement('div')
+                card.classList.add('card', 'col-lg-4', 'col-md-4', 'col-sm-6', 'text-center', 'produto', 'produtoClick')
+                card.setAttribute('data-id', item.id)
+
+                card.addEventListener('click', function () {
+                    // Obter o ID único do atributo de dados do card
+                    var id = this.getAttribute('data-id');
+                    localStorage.setItem('idDetalhes', id);
+                    var url = './detalhes.html?id=' + id
+                    window.location.href = url;
+                });
+
+                let cardBody = document.createElement('div')
+                cardBody.classList.add('card-body')
+
+                let imagem = document.createElement('img')
+                imagem.src = item.image
+                imagem.alt = item.title
+                imagem.classList.add('card-img-top')
+
+                let titulo = document.createElement('h5')
+                titulo.classList.add('card-title')
+                titulo.textContent = item.title;
+
+                let preco = document.createElement('p')
+                preco.classList.add('card-text')
+                preco.textContent = 'Preço: $' + item.price
+
+                // Adicionar elementos ao card
+                cardBody.appendChild(titulo)
+                cardBody.appendChild(preco)
+
+                card.appendChild(imagem)
+                card.appendChild(cardBody)
+
+                // Adicionar o card ao container
+                container.appendChild(card)
+            });
+        })
+        .catch(error => {
+            console.log('Ocorreu um erro ao pesquisar pela categoria:', error)
         })
 }
